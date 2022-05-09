6c3fa3328b721f63a9a9561d5b5ea75b2f5fde4d
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2022-05-09 13:26:14 +0100

    Initial commit

---
+++ src/main/java/pt/ua/deti/codespell/App.java
@@ -0,0 +1,17 @@
+package pt.ua.deti.codespell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+
+        // INJECT CODE HERE
+
+    }
+
+}
